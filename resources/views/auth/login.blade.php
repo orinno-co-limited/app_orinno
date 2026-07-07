96c0fb306405adfd3120fe98fa41d9f57923f98c
Tash <treshobya@gmail.com>
2026-07-08 01:10:00 +0300

    fixing the broken UI around the TAC

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -145,12 +145,16 @@
                                         @endif
                                     </div>
                                 </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <p class="font-14 mt-15 mb-0">
+                                            {{ __('By signing in, you agree to our') }}
+                                            <a href="{{ route('terms-conditions') }}" target="_blank"
+                                                class="secondary-color font-medium">{{ __('Terms & Conditions') }}</a>.
+                                        </p>
+                                    </div>
+                                </div>
                             </form>
-                            <p class="font-14 mt-25">
-                                {{ __('By signing in, you agree to our') }}
-                                <a href="{{ route('terms-conditions') }}" target="_blank"
-                                    class="secondary-color font-medium">{{ __('Terms & Conditions') }}</a>.
-                            </p>
                         </div>
                     </div>
                     <div class="col-md-6">
